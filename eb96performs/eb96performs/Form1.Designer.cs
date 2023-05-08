6edb83a7889bb616e0f076a78d1201c40b29a1c0
Emir9396 <121495099+Emir9396@users.noreply.github.com>
2023-05-08 22:13:02 +0300

    proje

---
+++ eb96performs/eb96performs/Form1.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace eb96performs
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label11 = new Label();
+            label8 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label9 = new Label();
+            label14 = new Label();
+            label10 = new Label();
+            label15 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = SystemColors.Control;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.GridColor = SystemColors.ActiveCaption;
+            dataGridView1.Location = new Point(3, 2);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(407, 318);
+            dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(3, 369);
+            button1.Name = "button1";
+            button1.Size = new Size(111, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Giriş Kaydet";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.ForeColor = SystemColors.ActiveCaptionText;
+            button2.Location = new Point(120, 369);
+            button2.Name = "button2";
+            button2.Size = new Size(111, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Sil";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(3, 340);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(111, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(120, 340);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(111, 23);
+            textBox2.TabIndex = 2;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(237, 341);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(111, 23);
+            textBox3.TabIndex = 2;
+            textBox3.Text = "0";
+            textBox3.TextChanged += textBox2_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 322);
+            label1.Name = "label1";
+            label1.Size = new Size(35, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Plaka";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(120, 323);
+            label2.Name = "label2";
+            label2.Size = new Size(58, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Giriş Saati";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = SystemColors.ButtonHighlight;
+            label3.Location = new Point(425, 2);
+            label3.Name = "label3";
+            label3.Size = new Size(72, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Saatlik Ücret";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(237, 323);
+            label4.Name = "label4";
+            label4.Size = new Size(60, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Çıkış Saati";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(354, 341);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(111, 23);
+            textBox4.TabIndex = 2;
+            textBox4.Text = "0";
+            textBox4.TextChanged += textBox2_TextChanged;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(354, 322);
+            label5.Name = "label5";
+            label5.Size = new Size(35, 15);
+            label5.TabIndex = 3;
+            label5.Text = "Ücret";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.ForeColor = SystemColors.ButtonHighlight;
+            label6.Location = new Point(425, 28);
+            label6.Name = "label6";
+            label6.Size = new Size(66, 15);
+            label6.TabIndex = 4;
+            label6.Text = "15 Dk beleş";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.ForeColor = SystemColors.ButtonHighlight;
+            label7.Location = new Point(425, 43);
+            label7.Name = "label7";
+            label7.Size = new Size(68, 15);
+            label7.TabIndex = 4;
+            label7.Text = "30 Dk=15TL";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.ForeColor = SystemColors.ButtonHighlight;
+            label11.Location = new Point(425, 103);
+            label11.Name = "label11";
+            label11.Size = new Size(70, 15);
+            label11.TabIndex = 4;
+            label11.Text = "3 Saat=90TL";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.ForeColor = SystemColors.ButtonHighlight;
+            label8.Location = new Point(425, 58);
+            label8.Name = "label8";
+            label8.Size = new Size(70, 15);
+            label8.TabIndex = 4;
+            label8.Text = "1 Saat=30TL";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.ForeColor = SystemColors.ButtonHighlight;
+            label12.Location = new Point(425, 118);
+            label12.Name = "label12";
+            label12.Size = new Size(76, 15);
+            label12.TabIndex = 4;
+            label12.Text = "4 Saat=120TL";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.ForeColor = SystemColors.ButtonHighlight;
+            label13.Location = new Point(425, 133);
+            label13.Name = "label13";
+            label13.Size = new Size(79, 15);
+            label13.TabIndex = 4;
+            label13.Text = "5 Saat=150 TL";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.ForeColor = SystemColors.ButtonHighlight;
+            label9.Location = new Point(425, 73);
+            label9.Name = "label9";
+            label9.Size = new Size(85, 15);
+            label9.TabIndex = 4;
+            label9.Text = "1.30 Saat=45TL";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.ForeColor = SystemColors.ButtonHighlight;
+            label14.Location = new Point(425, 148);
+            label14.Name = "label14";
+            label14.Size = new Size(92, 15);
+            label14.TabIndex = 4;
+            label14.Text = "1 Gecelik=300TL";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.ForeColor = SystemColors.ButtonHighlight;
+            label10.Location = new Point(425, 88);
+            label10.Name = "label10";
+            label10.Size = new Size(70, 15);
+            label10.TabIndex = 4;
+            label10.Text = "2 Saat=60TL";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.ForeColor = SystemColors.ButtonHighlight;
+            label15.Location = new Point(425, 163);
+            label15.Name = "label15";
+            label15.Size = new Size(92, 15);
+            label15.TabIndex = 4;
+            label15.Tag = "1 Günlük=720TL";
+            label15.Text = "1 Günlük=720TL";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.pala4;
+            pictureBox1.Location = new Point(413, 258);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(177, 80);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.pala1;
+            pictureBox2.Location = new Point(413, 181);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(84, 80);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.pala2;
+            pictureBox4.Location = new Point(492, 181);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(98, 80);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 5;
+            pictureBox4.TabStop = false;
+            // 
+            // button3
+            // 
+            button3.ForeColor = SystemColors.ActiveCaptionText;
+            button3.Location = new Point(506, 369);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 6;
+            button3.Text = "Çıkış";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            BackColor = Color.DimGray;
+            ClientSize = new Size(583, 394);
+            Controls.Add(button3);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label15);
+            Controls.Add(label10);
+            Controls.Add(label14);
+            Controls.Add(label9);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label8);
+            Controls.Add(label11);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label3);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            ForeColor = SystemColors.ButtonHighlight;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Cabbar Oğulları Otopark AŞ.";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label11;
+        private Label label8;
+        private Label label12;
+        private Label label13;
+        private Label label9;
+        private Label label14;
+        private Label label10;
+        private Label label15;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox4;
+        private Button button3;
+    }
+}
